0d5ca4bec8af2cdc0db4e0373940313b842df99a
Gabriel <gbatschauer@gmail.com>
2025-04-02 17:21:58 -0300

    feat-create table

--- client/src/pages/Usuarios.jsx
+++ client/src/pages/Usuarios.jsx
@@ -42,37 +42,57 @@ function Usuarios() {
       <div className="w-[100%] sm:w-[80%] lg:w-[60%] pt-6">
         <Button onClick={novoUsuario}>Novo Usuário</Button>
       </div>
-      <div className="w-full justify-items-center space-y-2">
-        {usuarios.map((usuarios) => (
-          <div
-            className="flex flex-col md:flex-row space-y-2 md:space-y-0 bg-slate-400 p-5 w-[100%] sm:w-[80%] lg:w-[60%] justify-between rounded-lg py-3 text-white"
-            key={usuarios.id}
-          >
-            <div className="flex flex-col w-[85%] xl:flex-row ">
-              <p className="w-full xl:w-[30%]">{usuarios.nome}</p>
-              <p className="w-full xl:w-[50%]">{usuarios.email}</p>
-              <p className="w-full xl:w-[20%]">{usuarios.fone}</p>
-            </div>
-            <div className="flex">
-              <button
-                onClick={() => {
-                  handleDelete(usuarios.id);
-                }}
-                title="Deletar"
-              >
-                <Trash2Icon className="p-[2px] hover:p-[1px] transition-all" />
-              </button>
-              <button
-                onClick={() => {
-                  atualizarUsuario(usuarios);
-                }}
-                title="Editar"
-              >
-                <ForwardIcon className="p-[2px] hover:p-[0px] transition-all" />
-              </button>
-            </div>
-          </div>
-        ))}
+      <div className="w-[100%] sm:w-[80%] lg:w-[60%] justify-items-center space-y-2">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Nome
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Telefone
+              </th>
+              <th scope="col" className="px-6 py-3">
+                {" "}
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+            {usuarios.map((usuarios) => (
+              <tr className="border-b border-gray-700">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {usuarios.nome}
+                </th>
+                <th className="px-2 py-4">{usuarios.email}</th>
+                <th className="px-2 py-4">{usuarios.fone}</th>
+                <th className="px-0 py-4">
+                  <button
+                    onClick={() => {
+                      handleDelete(usuarios.id);
+                    }}
+                    title="Deletar"
+                  >
+                    <Trash2Icon className="p-[2px] hover:p-[1px] transition-all" />
+                  </button>
+                  <button
+                    onClick={() => {
+                      atualizarUsuario(usuarios);
+                    }}
+                    title="Editar"
+                  >
+                    <ForwardIcon className="p-[2px] hover:p-[0px] transition-all" />
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
